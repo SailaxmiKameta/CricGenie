cdb2d1aea7bf3be5ef52d314b5024c68e23fb7dd
Pallampati, Saivenkatakrishna <142937151+saivenkatakrishna2@users.noreply.github.com>
2024-05-13 21:23:16 -0400

    Add code for AJAX call

--- CricGenie/players/urls.py
+++ CricGenie/players/urls.py
@@ -7,6 +7,5 @@ urlpatterns = [
     path('player/<int:player_id>/', views.player_details, name='player_details'),
     path('player/<int:player_id>/add_remove_favorite/', views.add_remove_favorite, name='add_remove_favorite'),
     path('favorite-players/', views.favorite_players, name='favorite_players'),
-    path('player/<int:player_id>/add_remove_favorite/', views.add_remove_favorite, name='add_remove_favorite'),
 
 ]
